e3e341580c0a7356246bcc410e59efadab35501d
MisterE123 <gbrrudmin@gmail.com>
2021-01-18 20:58:42 -0500

    Update rabbit.lua

--- mobs/rabbit.lua
+++ mobs/rabbit.lua
@@ -1,131 +1,125 @@
---License for code WTFPL and otherwise stated in readmes
-mobs:register_mob("mobs_creatures:rabbit",{
-	type = "animal",
-	passive = true,
-	reach = 1,
-	hp_min = 3,
-	hp_max = 3,
-	collisionbox = {-0.2, -0.01, -0.2, 0.2, 0.49, 0.2},
-	visual = "mesh",
-	mesh = "mobs_creatures_rabbit.b3d",
-	textures = {
-        {"mobs_creatures_rabbit_brown.png"},
-        {"mobs_creatures_rabbit_gold.png"},
-        {"mobs_creatures_rabbit_white.png"},
-        {"mobs_creatures_rabbit_white_splotched.png"},
-        {"mobs_creatures_rabbit_salt.png"},
-        {"mobs_creatures_rabbit_black.png"},
-	{"mobs_creatures_rabbit_toast.png"},
+
+local function rabbit_logic(self)
+
+	if self.hp <= 0 then
+		mob_core.on_die(self)
+		return
+	end
+	local prty = mobkit.get_queue_priority(self)
+	local player = mobkit.get_nearby_player(self)
+
+	if mobkit.timer(self,1) then
+
+		mob_core.vitals(self)
+		mob_core.random_drop(self, 10, 1800, "mobs_creatures:poop_turd")
+		mob_core.random_sound(self, 8)
+
+
+		
+        if prty < 9 and player then
+			if self.isinliquid then --if for some reason we have been knocked into water, then we will swim away from the player
+				mob_core.hq_swimfrom(self, 10, player, 1)  --mob_core.hq_swimfrom(self, prty, target, speed), speed is a multiplier on normal speed
+			else
+				mob_core.hq_runfrom(self, 10, player)   --mob_core.hq_runfrom(self, prty, tgtobj)
+			end
+		end
+
+		if prty < 9 then
+			if self.is_in_liquid then
+				mob_core.hq_liquid_recovery(self, 9, 'walk') --mob_core.hq_liquid_recovery(self, prty, anim)
+			end
+		end
+
+		if mobkit.is_queue_empty_high(self) then
+			mob_core.hq_roam(self, 0)
+		end
+	end
+end
+
+
+minetest.register_entity("mobs_creatures:rabbit",{
+	physical = true, 
+	collide_with_objects = true,
+	collisionbox = {-0.2, -0.01, -0.2, 0.2, 0.49, 0.2}, --pulled from mobs_redo definition verbatim
+	visual_size = {x=1.5, y=1.5},						--pulled from mobs_redo definition verbatim, if present
+	visual = "mesh",                                    --pulled from mobs_redo definition verbatim
+	mesh = "mobs_creatures_rabbit.b3d",                 --pulled from mobs_redo definition verbatim
+	textures = {"mobs_creatures_rabbit_brown.png",
+				"mobs_creatures_rabbit_gold.png",
+				"mobs_creatures_rabbit_white.png",
+				"mobs_creatures_rabbit_white_splotched.png",
+				"mobs_creatures_rabbit_salt.png",
+				"mobs_creatures_rabbit_black.png",
+				"mobs_creatures_rabbit_toast.png"
+			},
+
+			--Woah, that is different from mobs_redo!
+
+	timeout = 500, --how long until the mob is removed, 0 for never
+	buoyancy = .7, 
+	lung_capacity = 5, --how many seconds it can hold its breath before taking water damage
+	max_hp = 3, -- we can take this from the mobs_redo def
+	on_step = mob_core.on_step,
+	on_activate = mob_core.on_activate,
+	get_staticdata = mobkit.statfunc,
+	logic = rabbit_logic,
+	animation = {									-- has to be translated
+		walk={range={x=0,y=20},speed=25,loop=true},
+		run={range={x=0,y=20},speed=50,loop=true},
+		stand={range={x=0,y=0},speed=25,loop=true},
 	},
-	visual_size = {x=1.5, y=1.5},
-	sounds = {
-	random = "mobs_creatures_rabbit_random",
-	jump = "mobs_creatures_rabbit_jump",
-	damage = "mobs_creatures_rabbit_pain",
-	death = "mobs_creatures_rabbit_death",	
+	sounds = {										--can be copied from redo
+		random = "mobs_creatures_rabbit_random",
+		jump = "mobs_creatures_rabbit_jump",
+		damage = "mobs_creatures_rabbit_pain",
+		death = "mobs_creatures_rabbit_death",	
 	},
-	makes_footstep_sound = false,
-	walk_velocity = 1,
-	run_velocity = 4,
-	floats = 1,
-	runaway = true,
-        runaway_from = {"player"},
-	jump = true,
+	max_speed = 4, --taken from mobs_redo's run_velocity
+	jump_height = 1.1,
+	stepheight = 1.1,
+	view_range = 8, --copied from mobs_redo
+	attack={range=2,damage_groups={fleshy=1}},
+	armor_groups = {fleshy=100},
+		
+
+	fall_damage = true,
+	
+	reach = 2,
+	damage = 1,
+	knockback = 0,
+	defend_owner = false,
 	drops = {
 		{name = "mobs_creatures:rabbit_raw", chance = 1, min = 0, max = 1},
 		{name = "mobs_creatures:rabbit_hide", chance = 1, min = 0, max = 1},
 	},
-	water_damage = 4,
-	lava_damage = 4,
-	light_damage = 0,
-	fear_height = 4,
-	animation = {
-		speed_normal = 25,		speed_run = 50,
-		stand_start = 0,		stand_end = 0,
-		walk_start = 0,		walk_end = 20,
-		run_start = 0,		run_end = 20,
-	},
-	-- Follow (yellow) dangelions, carrots and golden carrots
-	follow = {"default:apple","farming:carrot"},
-	view_range = 8,
-	-- Eat carrots and reduce their growth stage by 1
-	replace_rate = 10,
-	replace_what = {
-                -- Farming Redo carrots
-                {"farming:carrot_8", "farming:carrot_7", 0},
-                {"farming:carrot_7", "farming:carrot_6", 0},
-                {"farming:carrot_6", "farming:carrot_5", 0},
-                {"farming:carrot_5", "farming:carrot_4", 0},
-                {"farming:carrot_4", "farming:carrot_3", 0},
-                {"farming:carrot_3", "farming:carrot_2", 0},
-                {"farming:carrot_2", "farming:carrot_1", 0},
-                {"farming:carrot_1", "air", 0},
-		},
-	on_rightclick = function(self, clicker)
-		-- Feed, tame protect or capture
-		if mobs:feed_tame(self, clicker, 1, true, true) then return end
-		if mobs:protect(self, clicker) then return end
-		if mobs:capture_mob(self, clicker, 0, 50, 80, false, nil) then return end
+
+
+	obstacle_avoidance_range = 5,
+	surface_avoidance_range = 0,
+	floor_avoidance_range = 0,
+	on_punch = function(self, puncher, time_from_last_punch, tool_capabilities, dir)
+		if time_from_last_punch < .2 then return end --prevent jitterclicking 
+		mobkit.clear_queue_high(self)
+		mob_core.on_punch_basic(self, puncher, tool_capabilities, dir) --calls damage functions and flashes red
+		mob_core.hq_runfrom(self, 10, puncher)
 	end,
 })
--- Mob spawning rules.
--- Different skins depending on spawn location
 
-local spawn = {
+mob_core.register_spawn_egg("mobs_creatures:rabbit", "ab7e35", "26231f")
+
+
+mob_core.register_spawn({
 	name = "mobs_creatures:rabbit",
-	neighbors = {"air"},
-	chance = 8000,
-	active_object_count = 5,
-	min_light = 3,
+	nodes = {"group:crumbly"},
+	min_light = 0,
 	max_light = 15,
-	min_height = 1,
-	max_height = 200,
-}
-local spawn_desert = table.copy(spawn)
-spawn_desert.nodes = {"default:desert_sand", "group:sand"}
-spawn_desert.on_spawn = function(self, pos)
-	local texture = "mobs_creatures_rabbit_gold.png"
-	self.base_texture = {"mobs_creatures_rabbit_gold.png"}
-	self.object:set_properties({textures = self.base_texture})
-end
-mobs:spawn(spawn_desert)
-
-local spawn_snow = table.copy(spawn)
-spawn_snow.nodes = {"default:snow", "default:snowblock", "default:dirt_with_snow"}
-spawn_snow.on_spawn = function(self, pos)
-	local texture
-	local r = math.random(1, 100)
-	-- 80% white fur
-	if r <= 80 then
-		texture = "mobs_creatures_rabbit_white.png"
-	-- 20% black and white fur
-	else
-		texture = "mobs_creatures_rabbit_white_splotched.png"
-	end
-	self.base_texture = { texture }
-	self.object:set_properties({textures = self.base_texture})
-end
-mobs:spawn(spawn_snow)
-
-local spawn_grass = table.copy(spawn)
-spawn_grass.nodes = {"default:dirt_with_grass","ethereal:prairie_dirt"}
-spawn_grass.on_spawn = function(self, pos)
-	local texture
-	local r = math.random(1, 100)
-	-- 50% brown fur
-	if r <= 50 then
-		texture = "mobs_creatures_rabbit_brown.png"
-	-- 40% salt fur
-	elseif r <= 90 then
-		texture = "mobs_creatures_rabbit_salt.png"
-	-- 10% black fur
-	else
-		texture = "mobs_creatures_rabbit_black.png"
-	end
-	self.base_texture = { texture }
-	self.object:set_properties({textures = self.base_texture})
-end
-mobs:spawn(spawn_grass)
-
--- Spawn egg
-mobs:register_egg("mobs_creatures:rabbit", "Rabbit Spawn Egg", "wool_brown.png", 1)
+	min_height = -100,
+	max_height = 500,
+	min_rad = 24,
+	max_rad = 256,
+	group = 2,
+	optional = {
+		
+		reliability = 3,
+	}
+}, 16, 6)
